547349d052d99f95687df9d6315a738d31576832
Svitlana <G00473000@atu.ie>
2025-10-16 15:54:49 +0100

    Enhance bank holiday program with unique date check
    Added functionality to check for unique bank holiday dates in Northern Ireland compared to the rest of the UK.

--- assignments/assignment02-bankholdiays.py
+++ assignments/assignment02-bankholdiays.py
@@ -13,4 +13,28 @@ for event in data['northern-ireland']['events']:
     if 'bank' in event['title'].lower():
         bank_holidays_dates.append(event['date'])
 
+
 print(bank_holidays_dates)
+
+
+
+
+# tricky part where we check on unique date for northern Ireland
+
+uk_bank_holidays_dates = []
+for region in data:
+    if region != 'northern-ireland':
+        for event in data[region]['events']:
+            if event['date'] not in uk_bank_holidays_dates:   
+                uk_bank_holidays_dates.append(event['date'])
+
+print("Other UK bank holiday dates:", uk_bank_holidays_dates)
+
+
+unique_bank_holidays_ie = []
+for event in data['northern-ireland']['events']:
+    date = event['date']
+    if date not in uk_bank_holidays_dates:
+        unique_bank_holidays_ie.append(date)
+
+print("Unique Northern Ireland bank holiday dates:", unique_bank_holidays_ie)
